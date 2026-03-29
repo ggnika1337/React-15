a56a0ff6333e3dc54ae7fe17e286825461793700
ggnika1337 <nikushanika568@gmail.com>
2026-03-29 22:42:47 +0400

    Feat: Added tvseries, movies page (with search)

--- src/components/__organisms/Browse/Movies.jsx
+++ src/components/__organisms/Browse/Movies.jsx
@@ -1,7 +1,62 @@
-import React from "react";
+import React, { useState } from "react";
+import data from "../../../../data.json";
+import Movie from "../../__atoms/Movie/Movie";
+import Search from "../../__atoms/Search/Search";
 
 function Movies({ MoviesDisplay }) {
-  return <div className={`text-white ${MoviesDisplay}`}>Movies</div>;
+  const [search, setSearch] = useState("");
+  let searched = data.filter(
+    (el) => el.title.includes(search) && el.category === "Movie",
+  );
+
+  return (
+    <div
+      className={`text-white pb-[50px] w-full ${MoviesDisplay} flex-col gap-[30px]`}
+    >
+      <Search
+        onSubmit={(e) => {
+          e.preventDefault();
+          console.log(searched);
+          setEntered(true);
+        }}
+        onChange={(e) => {
+          setSearch(e.target.value);
+          console.log(search);
+        }}
+        placeholder={"Search for movies"}
+      />
+
+      <div className="flex flex-col">
+        <h1
+          style={{ display: search === "" ? "flex" : "none" }}
+          className="text-[32px] font-[400] text-white"
+        >
+          Movies
+        </h1>
+        <h1
+          className="font-[400] text-[32px]"
+          style={{ display: search === "" ? "none" : "block" }}
+        >
+          Found {searched.length} results for ‘{search}’
+        </h1>
+        <div className="text-white flex flex-wrap gap-[20px] w-full">
+          {searched.map((el, key) => {
+            return (
+              <Movie
+                key={key}
+                title={el.title}
+                year={el.year}
+                type={el.category}
+                rating={el.rating}
+                image={el.thumbnail.regular.large}
+                scroll={false}
+              />
+            );
+          })}
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Movies;
--- src/components/__organisms/Browse/TVShows.jsx
+++ src/components/__organisms/Browse/TVShows.jsx
@@ -1,7 +1,62 @@
-import React from "react";
+import React, { useState } from "react";
+import data from "../../../../data.json";
+import Movie from "../../__atoms/Movie/Movie";
+import Search from "../../__atoms/Search/Search";
 
-function TVShows({ TVDisplay }) {
-  return <div className={`text-white ${TVDisplay}`}>TV Shows</div>;
+function Movies({ TVDisplay }) {
+  const [search, setSearch] = useState("");
+  let searched = data.filter(
+    (el) => el.title.includes(search) && el.category === "TV Series",
+  );
+
+  return (
+    <div
+      className={`text-white pb-[50px] w-full ${TVDisplay} flex-col gap-[30px]`}
+    >
+      <Search
+        onSubmit={(e) => {
+          e.preventDefault();
+          console.log(searched);
+          setEntered(true);
+        }}
+        onChange={(e) => {
+          setSearch(e.target.value);
+          console.log(search);
+        }}
+        placeholder={"Search for movies"}
+      />
+
+      <div className="flex flex-col">
+        <h1
+          style={{ display: search === "" ? "flex" : "none" }}
+          className="text-[32px] font-[400] text-white"
+        >
+          TV Series
+        </h1>
+        <h1
+          className="font-[400] text-[32px]"
+          style={{ display: search === "" ? "none" : "block" }}
+        >
+          Found {searched.length} results for ‘{search}’
+        </h1>
+        <div className="text-white flex flex-wrap gap-[20px] w-full">
+          {searched.map((el, key) => {
+            return (
+              <Movie
+                key={key}
+                title={el.title}
+                year={el.year}
+                type={el.category}
+                rating={el.rating}
+                image={el.thumbnail.regular.large}
+                scroll={false}
+              />
+            );
+          })}
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default TVShows;
+export default Movies;
