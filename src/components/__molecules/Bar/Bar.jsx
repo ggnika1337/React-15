e69d15e2d0534477c653604c85d37af59e2f6d15
ggnika1337 <nikushanika568@gmail.com>
2026-03-31 15:37:55 +0400

    Fix: responsive design

--- src/components/__molecules/Bar/Bar.jsx
+++ src/components/__molecules/Bar/Bar.jsx
@@ -12,7 +12,7 @@ function Bar({ onAllClick, onMoviesClick, onTVClick, onFavouritesClick }) {
       <div className="bg-[#161D2F] px-[28px] py-[32px] w-[96px] h-[90vh] rounded-[20px] items-center flex justify-between flex-col max-md:rounded-[0px] max-md:w-full max-md:h-[56px] max-md:py-[18px] max-md:flex-row max-md:absolute left-0 top-0">
         <div className="flex flex-col gap-[75px] max-md:flex-row max-md:gap-0 max-md:w-full">
           <img className="cursor-pointer" src={Film} />
-          <div className="flex flex-col items-center gap-[40px] max-md:flex-row max-md:w-full max-md:justify-center">
+          <div className="flex flex-col items-center gap-[40px] max-md:gap-[24px] max-md:flex-row max-md:w-full max-md:justify-center">
             <img
               onClick={onAllClick}
               src={All}
